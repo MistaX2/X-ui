f641cb7f3baaa7c02c7e4955b273b28a67ca0e50
Mr.-x- <mistax2.0z@gmail.com>
2025-07-17 21:41:21 +0530

    Update x-ui.sh

--- x-ui.sh
+++ x-ui.sh
@@ -134,7 +134,7 @@ before_show_menu() {
 }
 
 install() {
-    bash <(curl -Ls https://raw.githubusercontent.com/MistaX2/X-ui/refs/heads/main/x-ui.sh)
+    bash <(curl -Ls https://raw.githubusercontent.com/MistaX2/X-ui/refs/heads/main/install.sh)
     if [[ $? == 0 ]]; then
         if [[ $# == 0 ]]; then
             start
